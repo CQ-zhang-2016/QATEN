7c4e2797e031a1a8b9f8c20be64203830c49fd3e
Husky <38036273+CQ-zhang-2016@users.noreply.github.com>
2022-08-27 16:18:47 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,5 @@
-import sys
-from subprocess import call
 import torch
 import config as cfg
-
-class Logger(object):
-	"""Writes both to file and terminal"""
-	def __init__(self, savepath, mode='a'):
-		self.terminal = sys.stdout
-		self.log = open(savepath + 'logfile.log', mode)
-
-	def write(self, message):
-		self.terminal.write(message)
-		self.log.write(message)
-
-	def flush(self):
-		# this flush method is needed for python 3 compatibility.
-		# this handles the flush command by doing nothing.
-		# you might want to specify some extra behavior here.
-		pass
-
 
 class Normalizer(object):
 	"""Normalize a Tensor and restore it later. """
@@ -45,37 +26,6 @@ class Normalizer(object):
 		self.mean = state_dict['mean']
 		self.std = state_dict['std']
 
-
-class AverageMeter(object):
-	"""
-	Computes and stores the average and current value. Accomodates both numbers and tensors.
-	If the input to be monitored is a tensor, also need the dimensions/shape of the tensor.
-	Also, for tensors, it keeps a column wise count for average, sum etc.
-	"""
-	def __init__(self, is_tensor=False, dimensions=None):
-		if is_tensor and dimensions is None:
-			print('Bad definition of AverageMeter!')
-			sys.exit(1)
-		self.is_tensor = is_tensor
-		self.dimensions = dimensions
-		self.reset()
-
-	def reset(self):
-		self.count = 0
-		if self.is_tensor:
-			self.val = torch.zeros(self.dimensions, device=cfg.device)
-			self.avg = torch.zeros(self.dimensions, device=cfg.device)
-			self.sum = torch.zeros(self.dimensions, device=cfg.device)
-		else:
-			self.val = 0
-			self.avg = 0
-			self.sum = 0
-
-	def update(self, val, n=1):
-		self.val = val
-		self.sum += val * n
-		self.count += n
-		self.avg = self.sum / self.count
 
 def count_parameters(model):
 	return sum(p.numel() for p in model.parameters() if p.requires_grad)
